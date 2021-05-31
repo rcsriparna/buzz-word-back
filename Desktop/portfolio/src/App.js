97669c8890d03237cc776e1b84dbf0f3121010f1
Sriparna <moujay2015@gmail.com>
2021-05-31 19:29:34 +0100

    first commit

---
+++ Desktop/portfolio/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header"><h1>Welcome to my Page</h1></header>
+      <div className="nav-left"></div>
+      <div className="main">
+        <div className="flex">
+          <div>Node.js fruit machine</div>
+          <div>Fast Food ordering system </div>
+          <div>BC Graphic Ecxecise</div>
+          <div>HTML Fruit machine</div>
+          <div>Covid Registration</div>
+          <div><a href="https://github.com/rcsriparna/Horse">Horse Racing Game</a></div>
+        <div>Find My Dream Home</div>
+  
+        </div>
+      </div>
+      <div className="nav-right"></div>
+      <footer className="footer"></footer>
+       
+    </div>
+  );
+}
+
+export default App;
