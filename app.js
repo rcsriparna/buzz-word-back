052474d8260649b63c19a936c79e9240914dc960
Adam <adamrzymski@gmail.com>
2021-06-02 20:07:15 +0100

    Added fetch dependency cleared a bit code.

--- app.js
+++ app.js
@@ -1,64 +1,35 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const app=express()  
+const fetch = require('node-fetch');
+const app = express();
 
-const PORT = 3000;  
+const PORT = 3000;
 
-app.use(express.json())  
-app.use(cors())          
-app.use(express.static(__dirname));  
-
-
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname));
 
 const connect = async () => {
-  
-  app.listen(PORT, () => console.log(`Server is running on ${PORT}`));  
+  app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
 };
 
 const checkWord = async (req, res, next) => {
-  
-    try {
-         
-          
-          const word = req.params.word
-          
-          let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`, {
-              method: "GET", headers: {
-                   'Accept': 'application/json', 'Content-Type': 'application/json' 
-              }                
-          })
-          let result = await response.json()
-console.log(result);
+  const word = req.params.word;
+  let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  res.result = await response.json();
+  next();
+};
 
+const outputResult = (req, res, next) => {
+  console.log(res.result);
+};
 
-
-
-
-
-
-
-          fetch (!res.member) {
-            //if there are no matches ...
-            res.status(404).json({ message: "Cannot find member." });
-            return 
-          }
-  
-    } catch (e) {
-        res.status(500).json({ message: e.message });
-          return 
-    }
-      
-    next()  
-    
-  };
-
-  app.get("/api/:word", checkWord, outputMember);  
-
-
-
-
-
-
-
+app.get("/api/:word", checkWord, outputResult);
 
 connect();
