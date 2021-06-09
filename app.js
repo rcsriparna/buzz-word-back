671a4a8e748e350ce3ac9c443cc4d871ed0c1952
Adam <adamrzymski@gmail.com>
2021-06-09 18:43:19 +0100

    Improved letters generation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const app = express();
 
 const PORT = 3000;
@@ -15,51 +15,58 @@ const connect = async () => {
 
 const checkWord = async (req, res, next) => {
   const word = req.params.word;
-  let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`)
+  let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`);
 
   res.result = await response.json();
   next();
 };
 
 const outputResponse = (req, res, next) => {
-  res.json(res.result)
+  res.json(res.result);
 };
 
-const getRandomNumber = (max) =>{
-  Math.floor(Math.random()*max)
-}
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
-const returnedLetters = 
+const getLetter = (perc) => {
+  if (perc <= 12.02) return "E";
+  else if (perc > 12.02 && perc <= 21.12) return "T";
+  else if (perc > 21.12 && perc <= 29.24) return "A";
+  else if (perc > 29.24 && perc <= 36.92) return "O";
+  else if (perc > 36.92 && perc <= 44.23) return "I";
+  else if (perc > 44.23 && perc <= 51.18) return "N";
+  else if (perc > 51.18 && perc <= 57.46) return "S";
+  else if (perc > 57.46 && perc <= 63.48) return "R";
+  else if (perc > 63.48 && perc <= 69.4) return "H";
+  else if (perc > 69.4 && perc <= 73.72) return "D";
+  else if (perc > 73.72 && perc <= 77.7) return "L";
+  else if (perc > 77.7 && perc <= 80.58) return "U";
+  else if (perc > 80.58 && perc <= 83.29) return "C";
+  else if (perc > 83.29 && perc <= 85.9) return "M";
+  else if (perc > 85.9 && perc <= 88.2) return "F";
+  else if (perc > 88.2 && perc <= 90.31) return "Y";
+  else if (perc > 90.31 && perc <= 92.4) return "W";
+  else if (perc > 92.4 && perc <= 94.43) return "G";
+  else if (perc > 94.43 && perc <= 96.25) return "P";
+  else if (perc > 96.25 && perc <= 97.74) return "B";
+  else if (perc > 97.74 && perc <= 98.85) return "V";
+  else if (perc > 98.85 && perc <= 99.54) return "K";
+  else if (perc > 99.54 && perc <= 99.71) return "X";
+  else if (perc > 99.71 && perc <= 99.82) return "Q";
+  else if (perc > 99.82 && perc <= 99.92) return "J";
+  else return "Z";
+};
 
-for (let index = 0; index < numLetters; index++) {
-  getRandomNumber(100)
-  
-}
-
-
-
-()-[A, E, I, O, U, L, N, S, T, R]
-()-[D, G]
-()-[B, C, M, P]
-()-[F, H, V, W, Y]
-()-[K]
-()-[J, X]
-()-[Q, Z]
-
-10	30
-8	24
-5	15
-4	12
-3	9
-2	6
-1	3
-
-
-
-
+const generateLetters = (req, res, next) => {
+  const letters = [];
+  for (let i = 0; i < req.params.size; i++) {
+    letters.push(getLetter(getRandomNumber(100)))
+  }
+  res.result = letters;
+  next();
+};
 
 //http://localhost:3000/api/dict/<word>
-//response 
+//response
 /* 
 [Object{
   "word":String("forward"),
@@ -81,5 +88,6 @@ for (let index = 0; index < numLetters; index++) {
   "example":String:("my emails were forwarded to a friend")
 }] */
 app.get("/api/dict/:word", checkWord, outputResponse);
+app.get("/api/rndletters/:size", generateLetters, outputResponse);
 
 connect();
