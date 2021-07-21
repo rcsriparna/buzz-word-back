95a4792be797f22331c1fe391d49b7ef16146919
Adam <adamrzymski@gmail.com>
2021-07-21 23:59:41 +0100

    Final

--- build/middlewares/rules.js
+++ build/middlewares/rules.js
@@ -13,7 +13,7 @@ var NAMESPACE = "CORS"; // API RULES | OPTIONS | CORS etc...
 
 var rulesMiddleware = function rulesMiddleware(req, res, next) {
   if (req.url != "/api/stateaa" && req.url != "/timesync") {
-    res.header("Access-Control-Allow-Origin", _config.config.server.hostname);
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Credentials", "true");
 
@@ -28,6 +28,7 @@ var rulesMiddleware = function rulesMiddleware(req, res, next) {
     if (req.method == "OPTIONS") {
       res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET, OPTIONS");
       res.header("Access-Control-Allow-Headers", "Content-Type");
+      res.header("Access-Control-Allow-Origin", "*");
 
       _logging.logger.info(NAMESPACE, "[RES-HEADERS] Access-Control-Allow-Methods: ".concat(res.get("Access-Control-Allow-Methods")));
 
